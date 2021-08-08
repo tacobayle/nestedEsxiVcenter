2ca331c19ebd4b16312ce7aacb36c20ce9f84e88
tacobayle <nicolas.bayle@gmail.com>
2021-08-08 07:00:44 +0000

    20210808

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "vsphere" {
-  user           = var.vsphere_user
+  user           = var.vsphere_username
   password       = var.vsphere_password
   vsphere_server = var.vcenter_underlay.server
   allow_unverified_ssl = true

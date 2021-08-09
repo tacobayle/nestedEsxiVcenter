7fc834bfbecd6c8ff11481e77f971b698ea244d5
tacobayle <nicolas.bayle@gmail.com>
2021-08-09 13:25:32 +0000

    20210809

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 resource "dns_a_record_set" "esxi" {
   depends_on = [vsphere_virtual_machine.dnsntp]
-  count = length(var.vcenter_underlay.networks.management.esxi_ips)
+  count = (var.dns-ntp.create == true ? length(var.vcenter_underlay.networks.management.esxi_ips) : 0)
   zone  = "${var.dns.domain}."
   name  = "${var.esxi.basename}-${count.index}"
   addresses = [element(var.vcenter_underlay.networks.management.esxi_ips, count.index)]
@@ -9,7 +9,7 @@ resource "dns_a_record_set" "esxi" {
 
 resource "dns_ptr_record" "esxi" {
   depends_on = [vsphere_virtual_machine.dnsntp]
-  count = length(var.vcenter_underlay.networks.management.esxi_ips)
+  count = (var.dns-ntp.create == true ? length(var.vcenter_underlay.networks.management.esxi_ips) : 0)
   zone = "${var.dns-ntp.bind.reverse}.in-addr.arpa."
   name = split(".", element(var.vcenter_underlay.networks.management.esxi_ips, count.index))[3]
   ptr  = "${var.esxi.basename}-${count.index}.${var.dns.domain}."
@@ -17,6 +17,7 @@ resource "dns_ptr_record" "esxi" {
 }
 
 resource "dns_a_record_set" "vcenter" {
+  count = (var.dns-ntp.create == true ? 1 : 0)
   depends_on = [vsphere_virtual_machine.dnsntp]
   zone  = "${var.dns.domain}."
   name  = var.vcenter.name
@@ -25,6 +26,7 @@ resource "dns_a_record_set" "vcenter" {
 }
 
 resource "dns_ptr_record" "vcenter" {
+  count = (var.dns-ntp.create == true ? 1 : 0)
   depends_on = [vsphere_virtual_machine.dnsntp]
   zone = "${var.dns-ntp.bind.reverse}.in-addr.arpa."
   name = split(".", var.vcenter_underlay.networks.management.vcenter_ip)[3]

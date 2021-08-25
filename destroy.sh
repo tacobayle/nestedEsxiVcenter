4c539f0b54c9f92b32db2e936faaa485ae94c804
tacobayle <nicolas.bayle@gmail.com>
2021-08-25 09:12:13 +0000

    20210825

---
+++ destroy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Script to destroy the infrastructure
+#
+if [ -f "variables.json" ]; then
+  jsonFile="variables.json"
+else
+  exit 1
+fi
+#
+# Destroy DNS/NTP server on the underlay infrastructure
+#
+if [[ $(jq -c -r .dns-ntp.create $jsonFile) == true ]] ; then
+  cd dns-ntp
+  terraform destroy auto-approve -var-file=../$jsonFile
+  cd -
+fi
+#
+# Destroy the nested ESXi/vCenter infrastructure
+#
+terraform destroy -auto-approve -var-file=variables.json

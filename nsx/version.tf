5b1afb0290def34083c0cdb6fa88d8e9c199a4ac
tacobayle <nicolas.bayle@gmail.com>
2021-08-26 10:42:58 +0000

    20210826

--- nsx/version.tf
+++ nsx/version.tf
@@ -1,7 +1,7 @@
-terraform {
-  required_providers {
-    nsxt = {
-      source = "vmware/nsxt"
-    }
-  }
-}
+//terraform {
+//  required_providers {
+//    nsxt = {
+//      source = "vmware/nsxt"
+//    }
+//  }
+//}

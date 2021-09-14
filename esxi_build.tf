c05e97cd5bfbbfc5fd920317ee1cc5ec37c1b78a
tacobayle <nicolas.bayle@gmail.com>
2021-09-14 16:54:59 +0000

    20210914

--- esxi_build.tf
+++ esxi_build.tf
@@ -106,50 +106,50 @@ resource "vsphere_virtual_machine" "esxi_multiple_vswitch_wo_NSX_wo_Avi" {
   }
 }
 
-resource "vsphere_virtual_machine" "esxi_multiple_vswitch_wo_NSX_wo_Avi" {
-  depends_on = [ vsphere_file.iso_upload ]
-  count = (var.vcenter.dvs.single_vds == false && var.nsx.create == false && var.avi.create_network == false ? var.esxi.count : 0)
-  name             = "${var.esxi.basename}${count.index + 1}"
-  datastore_id     = data.vsphere_datastore.datastore.id
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  folder           = "/${var.vcenter_underlay.dc}/vm/${var.vcenter_underlay.folder}"
-
-  dynamic "network_interface" {
-    for_each = data.vsphere_network.esxi_networks
-    content {
-      network_id = network_interface.value["id"]
-    }
-  }
-
-  dynamic "network_interface" {
-    for_each = data.vsphere_network.esxi_networks
-    content {
-      network_id = network_interface.value["id"]
-    }
-  }
-
-  num_cpus = var.esxi.cpu
-  memory = var.esxi.memory
-  guest_id = var.esxi.guest_id
-  wait_for_guest_net_timeout = var.esxi.wait_for_guest_net_timeout
-  nested_hv_enabled = var.esxi.nested_hv_enabled
-  firmware = var.esxi.bios
-
-  dynamic "disk" {
-    for_each = var.esxi.disks
-    content {
-      size = disk.value["size"]
-      label = "${var.esxi.basename}${count.index + 1}-${disk.value["label"]}.lab_vmdk"
-      unit_number = disk.value["unit_number"]
-      thin_provisioned = disk.value["thin_provisioned"]
-    }
-  }
-
-  cdrom {
-    datastore_id = data.vsphere_datastore.datastore.id
-    path         = "isos/${basename(var.esxi.iso_location)}${count.index}.iso"
-  }
-}
+//resource "vsphere_virtual_machine" "esxi_multiple_vswitch_wo_NSX_wo_Avi" {
+//  depends_on = [ vsphere_file.iso_upload ]
+//  count = (var.vcenter.dvs.single_vds == false && var.nsx.create == false && var.avi.create_network == false ? var.esxi.count : 0)
+//  name             = "${var.esxi.basename}${count.index + 1}"
+//  datastore_id     = data.vsphere_datastore.datastore.id
+//  resource_pool_id = data.vsphere_resource_pool.pool.id
+//  folder           = "/${var.vcenter_underlay.dc}/vm/${var.vcenter_underlay.folder}"
+//
+//  dynamic "network_interface" {
+//    for_each = data.vsphere_network.esxi_networks
+//    content {
+//      network_id = network_interface.value["id"]
+//    }
+//  }
+//
+//  dynamic "network_interface" {
+//    for_each = data.vsphere_network.esxi_networks
+//    content {
+//      network_id = network_interface.value["id"]
+//    }
+//  }
+//
+//  num_cpus = var.esxi.cpu
+//  memory = var.esxi.memory
+//  guest_id = var.esxi.guest_id
+//  wait_for_guest_net_timeout = var.esxi.wait_for_guest_net_timeout
+//  nested_hv_enabled = var.esxi.nested_hv_enabled
+//  firmware = var.esxi.bios
+//
+//  dynamic "disk" {
+//    for_each = var.esxi.disks
+//    content {
+//      size = disk.value["size"]
+//      label = "${var.esxi.basename}${count.index + 1}-${disk.value["label"]}.lab_vmdk"
+//      unit_number = disk.value["unit_number"]
+//      thin_provisioned = disk.value["thin_provisioned"]
+//    }
+//  }
+//
+//  cdrom {
+//    datastore_id = data.vsphere_datastore.datastore.id
+//    path         = "isos/${basename(var.esxi.iso_location)}${count.index}.iso"
+//  }
+//}
 
 resource "vsphere_virtual_machine" "esxi_multiple_vswitch_with_NSX_with_Avi" {
   depends_on = [ vsphere_file.iso_upload ]
@@ -320,50 +320,50 @@ resource "vsphere_virtual_machine" "esxi_multiple_vswitch_wo_NSX_with_Avi" {
   }
 }
 
-resource "vsphere_virtual_machine" "esxi_multiple_vswitch_with_NSX_with_Avi" {
-  depends_on = [ vsphere_file.iso_upload ]
-  count = (var.vcenter.dvs.single_vds == false && var.nsx.create == true && var.avi.create_network == true ? var.esxi.count : 0)
-  name             = "${var.esxi.basename}${count.index + 1}"
-  datastore_id     = data.vsphere_datastore.datastore.id
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  folder           = "/${var.vcenter_underlay.dc}/vm/${var.vcenter_underlay.folder}"
-
-  dynamic "network_interface" {
-    for_each = data.vsphere_network.esxi_networks
-    content {
-      network_id = network_interface.value["id"]
-    }
-  }
-
-  dynamic "network_interface" {
-    for_each = data.vsphere_network.esxi_networks
-    content {
-      network_id = network_interface.value["id"]
-    }
-  }
-
-  num_cpus = var.esxi.cpu
-  memory = var.esxi.memory
-  guest_id = var.esxi.guest_id
-  wait_for_guest_net_timeout = var.esxi.wait_for_guest_net_timeout
-  nested_hv_enabled = var.esxi.nested_hv_enabled
-  firmware = var.esxi.bios
-
-  dynamic "disk" {
-    for_each = var.esxi.disks
-    content {
-      size = disk.value["size"]
-      label = "${var.esxi.basename}${count.index + 1}-${disk.value["label"]}.lab_vmdk"
-      unit_number = disk.value["unit_number"]
-      thin_provisioned = disk.value["thin_provisioned"]
-    }
-  }
-
-  cdrom {
-    datastore_id = data.vsphere_datastore.datastore.id
-    path         = "isos/${basename(var.esxi.iso_location)}${count.index}.iso"
-  }
-}
+//resource "vsphere_virtual_machine" "esxi_multiple_vswitch_with_NSX_with_Avi" {
+//  depends_on = [ vsphere_file.iso_upload ]
+//  count = (var.vcenter.dvs.single_vds == false && var.nsx.create == true && var.avi.create_network == true ? var.esxi.count : 0)
+//  name             = "${var.esxi.basename}${count.index + 1}"
+//  datastore_id     = data.vsphere_datastore.datastore.id
+//  resource_pool_id = data.vsphere_resource_pool.pool.id
+//  folder           = "/${var.vcenter_underlay.dc}/vm/${var.vcenter_underlay.folder}"
+//
+//  dynamic "network_interface" {
+//    for_each = data.vsphere_network.esxi_networks
+//    content {
+//      network_id = network_interface.value["id"]
+//    }
+//  }
+//
+//  dynamic "network_interface" {
+//    for_each = data.vsphere_network.esxi_networks
+//    content {
+//      network_id = network_interface.value["id"]
+//    }
+//  }
+//
+//  num_cpus = var.esxi.cpu
+//  memory = var.esxi.memory
+//  guest_id = var.esxi.guest_id
+//  wait_for_guest_net_timeout = var.esxi.wait_for_guest_net_timeout
+//  nested_hv_enabled = var.esxi.nested_hv_enabled
+//  firmware = var.esxi.bios
+//
+//  dynamic "disk" {
+//    for_each = var.esxi.disks
+//    content {
+//      size = disk.value["size"]
+//      label = "${var.esxi.basename}${count.index + 1}-${disk.value["label"]}.lab_vmdk"
+//      unit_number = disk.value["unit_number"]
+//      thin_provisioned = disk.value["thin_provisioned"]
+//    }
+//  }
+//
+//  cdrom {
+//    datastore_id = data.vsphere_datastore.datastore.id
+//    path         = "isos/${basename(var.esxi.iso_location)}${count.index}.iso"
+//  }
+//}
 
 resource "vsphere_virtual_machine" "esxi_single_vswitch" {
   depends_on = [ vsphere_file.iso_upload ]

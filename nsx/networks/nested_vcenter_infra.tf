7773babe719db9f4fd1500df398fadc0915257f8
tacobayle <nicolas.bayle@gmail.com>
2022-01-17 14:30:12 +0000

    20220117_nsx

--- nsx/networks/nested_vcenter_infra.tf
+++ nsx/networks/nested_vcenter_infra.tf
@@ -11,7 +11,7 @@ data "vsphere_host" "host_nested" {
 
 resource "vsphere_distributed_virtual_switch" "network_nsx_overlay" {
   count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true ? 1 : 0)
-  name = "${var.vcenter.dvs.portgroup.nsx_overlay.name}-vds"
+  name = var.vcenter.dvs.portgroup.nsx_overlay.name
   datacenter_id = data.vsphere_datacenter.dc_nested[0].id
   version = var.vcenter.dvs.version
   max_mtu = var.vcenter.dvs.portgroup.nsx_overlay.max_mtu
@@ -27,14 +27,14 @@ resource "vsphere_distributed_virtual_switch" "network_nsx_overlay" {
 
 resource "vsphere_distributed_port_group" "pg_nsx_overlay" {
   count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true ? 1 : 0)
-  name                            = var.vcenter.dvs.portgroup.nsx_overlay.name
+  name                            = "${var.vcenter.dvs.portgroup.nsx_overlay.name}-pg"
   distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.network_nsx_overlay[0].id
   vlan_id = 0
 }
 
 resource "vsphere_distributed_virtual_switch" "network_nsx_external" {
   count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true ? 1 : 0)
-  name = "${var.vcenter.dvs.portgroup.nsx_external.name}-vds"
+  name = var.vcenter.dvs.portgroup.nsx_external.name
   datacenter_id = data.vsphere_datacenter.dc_nested[0].id
   version = var.vcenter.dvs.version
   max_mtu = var.vcenter.dvs.portgroup.nsx_external.max_mtu
@@ -50,28 +50,7 @@ resource "vsphere_distributed_virtual_switch" "network_nsx_external" {
 
 resource "vsphere_distributed_port_group" "pg_nsx_external" {
   count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true ? 1 : 0)
-  name                            = var.vcenter.dvs.portgroup.nsx_external.name
+  name                            = "${var.vcenter.dvs.portgroup.nsx_external.name}-pg"
   distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.network_nsx_external[0].id
   vlan_id = 0
 }
-
-//resource "vsphere_distributed_virtual_switch" "network_nsx_overlay_wo_avi_networks" {
-//  count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true && var.avi.networks.create == false ? 1 : 0)
-//  name = "${var.vcenter.dvs.portgroup.nsx_overlay.name}_vds"
-//  datacenter_id = data.vsphere_datacenter.dc_nested[0].id
-//
-//  dynamic "host" {
-//    for_each = data.vsphere_host.host_nested
-//    content {
-//      host_system_id = host.value.id
-//      devices        = ["vmnic6"]
-//    }
-//  }
-//}
-//
-//resource "vsphere_distributed_port_group" "pg_nsx_overlay_wo_avi_networks" {
-//  count = (var.vcenter.dvs.single_vds == false && var.nsx.networks.create == true && var.avi.networks.create == false ? 1 : 0)
-//  name                            = var.vcenter.dvs.portgroup.nsx_overlay.name
-//  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.network_nsx_overlay[0].id
-//  vlan_id = 0
-//}

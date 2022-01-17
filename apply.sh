b4f98b5e440d91eece5351b2d440a2e52bbd4448
tacobayle <nicolas.bayle@gmail.com>
2022-01-17 06:40:17 +0000

    20220117_nsx

--- apply.sh
+++ apply.sh
@@ -72,7 +72,7 @@ fi
 # Build of the nested NSXT Manager
 #
 if [[ $(jq -c -r .nsx.manager.create $jsonFile) == true ]] || [[ $(jq -c -r .nsx.content_library.create $jsonFile) == true ]] ; then
-  tf_init_apply "Build of the nested NSXT Manager" nsx/manager ../../logs/tf_nsx.stdout ../../logs/tf_nsx.errors ../../$jsonFile
+  tf_init_apply "Build of the nested NSXT Manager - This should take less than 20 minutes" nsx/manager ../../logs/tf_nsx.stdout ../../logs/tf_nsx.errors ../../$jsonFile
   if [[ $(jq -c -r .nsx.manager.create $jsonFile) == true ]] ; then
     echo "waiting for 15 minutes to finish the NSXT bootsrap..."
     sleep 900

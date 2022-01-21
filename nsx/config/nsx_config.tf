7e4c9db741883b50aaf60609d0c261ff4705c197
tacobayle <nicolas.bayle@gmail.com>
2022-01-21 13:22:02 +0000

    20220121_nsx_config

--- nsx/config/nsx_config.tf
+++ nsx/config/nsx_config.tf
@@ -2,15 +2,29 @@ resource "null_resource" "ansible_init_manager" {
   provisioner "local-exec" {
     command = "ansible-playbook ansible/ansible_init_manager.yml -e @../../variables.json"
   }
+
+  provisioner "local-exec" {
+    command = "/bin/bash bash/register_compute_manager.sh"
+  }
+
+}
+
+resource "null_resource" "register_compute_manager" {
+  depends_on = [null_resource.ansible_init_manager]
+  provisioner "local-exec" {
+    command = "/bin/bash bash/register_compute_manager.sh"
+  }
+
 }
 
 resource "nsxt_policy_ip_pool" "pools" {
-  depends_on = [null_resource.ansible_init_manager]
+  depends_on = [null_resource.register_compute_manager]
   count = length(var.nsx.config.ip_pools)
   display_name = var.nsx.config.ip_pools[count.index].name
 }
 
 resource "nsxt_policy_ip_pool_static_subnet" "static_subnet" {
+  depends_on = [null_resource.register_compute_manager]
   count = length(var.nsx.config.ip_pools)
   display_name = "${var.nsx.config.ip_pools[count.index].name}-static-subnet"
   pool_path    = nsxt_policy_ip_pool.pools[count.index].path
@@ -25,13 +39,14 @@ resource "nsxt_policy_ip_pool_static_subnet" "static_subnet" {
 }
 
 data "nsxt_policy_transport_zone" "vlan_transport_zone" {
-  depends_on = [null_resource.ansible_init_manager]
+  depends_on = [null_resource.register_compute_manager]
   count = length(var.nsx.config.segments)
   display_name        = var.nsx.config.segments[count.index].transport_zone
 }
 
 
 resource "nsxt_policy_segment" "segments_for_single_vds" {
+  depends_on = [null_resource.register_compute_manager]
   count = (var.vcenter.dvs.single_vds == true && var.nsx.config.create == true ? length(var.nsx.config.segments) : 0)
   display_name        = var.nsx.config.segments[count.index].name
   vlan_ids = [var.nsx.config.segments[count.index].vlan]
@@ -40,6 +55,7 @@ resource "nsxt_policy_segment" "segments_for_single_vds" {
 }
 
 resource "nsxt_policy_segment" "segments_for_multiple_vds" {
+  depends_on = [null_resource.register_compute_manager]
   count = (var.vcenter.dvs.single_vds == false && var.nsx.config.create == true ? length(var.nsx.config.segments) : 0)
   display_name        = var.nsx.config.segments[count.index].name
   vlan_ids = ["0"]
@@ -47,18 +63,20 @@ resource "nsxt_policy_segment" "segments_for_multiple_vds" {
   description         = var.nsx.config.segments[count.index].description
 }
 
-resource "null_resource" "create_transport_zone_profiles" {
-  depends_on = [nsxt_policy_segment.segments_for_multiple_vds, nsxt_policy_segment.segments_for_single_vds, nsxt_policy_ip_pool_static_subnet.static_subnet]
+resource "null_resource" "create_transport_node_profiles" {
+  depends_on = [nsxt_policy_ip_pool.pools, nsxt_policy_ip_pool_static_subnet.static_subnet, nsxt_policy_segment.segments_for_single_vds, nsxt_policy_segment.segments_for_multiple_vds]
   provisioner "local-exec" {
     command = "ansible-playbook ansible/create_transport_zone_profiles.yml -e @../../variables.json"
   }
 }
 
-resource "null_resource" "register_compute_manager" {
-  depends_on = [null_resource.create_transport_zone_profiles]
+resource "null_resource" "create_host_transport_node" {
+  depends_on = [null_resource.create_transport_node_profiles]
+
   provisioner "local-exec" {
-    command = "/bin/bash bash/register_compute_manager.sh"
+    command = "/bin/bash bash/create_host_transport_node.sh"
   }
+
 }
 
 

dc8f957a5ff4d2bd1869ce38090d7bd79193c260
tacobayle <nicolas.bayle@gmail.com>
2022-01-23 16:31:50 +0000

    20220123_nsx_config

--- nsx/config/bash/create_host_transport_node.sh
+++ nsx/config/bash/create_host_transport_node.sh
@@ -29,3 +29,45 @@ do
   fi
 done
 curl -k -s -X POST -b cookies.txt -H "`grep X-XSRF-TOKEN headers.txt`" -H "Content-Type: application/json" -d '{"resource_type": "TransportNodeCollection", "display_name": "TransportNodeCollection-1", "description": "Transport Node Collections 1", "compute_collection_id": "'$compute_collection_external_id'", "transport_node_profile_id": "'$transport_node_profile_id'"}' https://$nsx_ip/api/v1/transport-node-collections
+discovered_nodes=$(curl -k -s -X GET -b cookies.txt -H "`grep X-XSRF-TOKEN headers.txt`" -H "Content-Type: application/json" https://$nsx_ip/policy/api/v1/infra/sites/default/enforcement-points/default/host-transport-nodes)
+IFS=$'\n'
+for item in $(echo $discovered_nodes | jq -c -r .results[])
+do
+  unique_id=$(echo $item | jq -c -r .unique_id)
+  retry=10
+  pause=60
+  attempt=0
+  while [[ "$(curl -k -s -X GET -b cookies.txt -o /dev/null -w ''%{http_code}'' -H "`grep X-XSRF-TOKEN headers.txt`" -H "Content-Type: application/json" https://$nsx_ip/policy/api/v1/infra/sites/default/enforcement-points/default/host-transport-nodes/$unique_id/state)" != "200" ]]; do
+    echo "waiting for transport node status HTTP code to be 200"
+    sleep $pause
+    ((attempt++))
+    if [ $attempt -eq $retry ]; then
+      echo "FAILED to get NSX Manager API to be ready after $retry"
+      exit 255
+    fi
+  done
+  retry=10
+  pause=60
+  attempt=0
+  while [[ "$(curl -k -s -X GET -b cookies.txt -H "`grep X-XSRF-TOKEN headers.txt`" -H "Content-Type: application/json" https://$nsx_ip/policy/api/v1/infra/sites/default/enforcement-points/default/host-transport-nodes/$unique_id/state | jq -r .deployment_progress_state.progress)" != 100 ]]; do
+    echo "waiting for transport node deployment progress at 100%"
+    sleep $pause
+    ((attempt++))
+    if [ $attempt -eq $retry ]; then
+      echo "FAILED to get transport node deployment progress at 100% after $retry"
+      exit 255
+    fi
+  done
+  retry=10
+  pause=60
+  attempt=0
+  while [[ "$(curl -k -s -X GET -b cookies.txt -H "`grep X-XSRF-TOKEN headers.txt`" -H "Content-Type: application/json" https://$nsx_ip/policy/api/v1/infra/sites/default/enforcement-points/default/host-transport-nodes/$unique_id/state | jq -r .state)" != "success" ]]; do
+    echo "waiting for transport node status success"
+    sleep $pause
+    ((attempt++))
+    if [ $attempt -eq $retry ]; then
+      echo "FAILED to get transport node status success after $retry"
+      exit 255
+    fi
+  done
+done
